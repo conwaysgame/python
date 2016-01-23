645652205f57e55f83ab15918bcf01cf2b63e474
Dan Hough <daniel.hough@gmail.com>
2016-01-23 01:27:08 +0000

    Started writing neighbour counter

--- gameoflifetest.py
+++ gameoflifetest.py
@@ -16,7 +16,14 @@ class ConwaysGame():
 
     def cell_is_alive(self, x, y):
         return self.map[x][y] == 1
-        
+
+    def number_of_live_neighbours(self, x, y):
+        live_neighbours = 0
+        for i in range(max(x - 1, 0), min(x + 1, self.width - 1)):
+            for j in range (max(y - 1, 0), min (y + 1, self.height - 1)):
+                if self.cell_is_alive(i, j) == True and (i != x and j != y) == True:
+                    live_neighbours += 1
+        return live_neighbours
 
 class ConwaysGameTests(unittest.TestCase):
     def test_set_world_size(self):
@@ -40,6 +47,25 @@ class ConwaysGameTests(unittest.TestCase):
         game.set_world_size(3, 1)
         self.assertRaises(IndexError, game.populate, 3, 0)
         self.assertRaises(IndexError, game.populate, 0, 1)
+
+'''
+OOOOO
+OOOOO
+XXOOO
+XOOOO
+OOOOO
+'''
+
+    def test_number_of_live_neighbours(self):
+        game = ConwaysGame()
+        game.set_world_size(5, 5)
+        game.populate(0, 2)
+        game.populate(0, 3)
+        game.populate(1, 2)
+        self.assertEqual(game.number_of_live_neighbours(1, 3), 3)
+        self.assertEqual(game.number_of_live_neighbours(1, 1), 2)
+        self.assertEqual(game.number_of_live_neighbours(2, 2), 1)
+        self.assertEqual(game.number_of_live_neighbours(0, 0), 0)
 
 
 if __name__ == '__main__':
