8185e74174177a4465d2ad236996a1f67aa22e4e
Dan Hough <daniel.hough@gmail.com>
2016-01-23 01:07:34 +0000

    Testing population and life reportage

--- gameoflifetest.py
+++ gameoflifetest.py
@@ -2,13 +2,21 @@ import unittest
 
 class ConwaysGame():
     def __init__(self):
+        self.map = [[]]
         self.width = 0
         self.height = 0
 
     def set_world_size(self, width, height):
         self.width = width
         self.height = height
+        self.map = [[0 for i in range(height)] for i in range(width)]
 
+    def populate(self, x, y):
+        self.map[x][y] = 1
+
+    def cell_is_alive(self, x, y):
+        return self.map[x][y] == 1
+        
 
 class ConwaysGameTests(unittest.TestCase):
     def test_set_world_size(self):
@@ -17,6 +25,16 @@ class ConwaysGameTests(unittest.TestCase):
         game.set_world_size(10, 5)
         self.assertEqual(game.width, 10)
         self.assertEqual(game.height, 5)
+    
+    def test_populate_and_cell_is_alive(self):
+        game = ConwaysGame()
+        game.set_world_size(10,10)
+        game.populate(1, 2)
+        game.populate(7, 4)
+        self.assertEqual(game.cell_is_alive(2, 2), False)
+        self.assertEqual(game.cell_is_alive(1, 2), True)
+        self.assertEqual(game.cell_is_alive(7, 4), True)
+
 
 if __name__ == '__main__':
     unittest.main()
